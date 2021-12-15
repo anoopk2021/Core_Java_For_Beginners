483cf11d878809d9c97a715e34bc0cf62d4754b3
Anoopk2021 <anoop.k.2017@gmail.com>
2021-12-15 13:52:22 +0530

    Variable_Demo

---
+++ src/variables/VariablesDemo.java
@@ -0,0 +1,27 @@
+package variables;
+
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+        String name= " Mohit Kumar";
+        String cityName= " Agra";
+        int age= 28;
+        int marks= 69;
+        double salary= 234234.345;
+        boolean flag=true;
+        short temperature= -200;
+        float number= -42.3f;
+        char letter= '\u0051';
+        // The Unicode Value of Q is \u0051, Hence we get Q as the output.
+
+        System.out.println(" Printing String Name " + name);
+        System.out.println(" Printing String City Name " + cityName);
+        System.out.println(" Printing Age " + age);
+        System.out.println(" Printing Marks " + marks);
+        System.out.println(" Printing Salary " + salary);
+        System.out.println(" Printing Flag " + flag);
+        System.out.println(" Printing Short Data Type " + temperature);
+        System.out.println(" Printing Float data Type " + number);
+        System.out.println(" Printing Char Data Type " + letter);
+    }
+}
