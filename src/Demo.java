0800ce655e19e3b61b6366dfd23f2b9d894e9efb
Anoopk2021 <anoop.k.2017@gmail.com>
2021-12-15 12:57:40 +0530

    Demo For Beginners

---
+++ src/Demo.java
@@ -0,0 +1,20 @@
+public class Demo {
+
+    /*  { }  Curly Bracket
+        [ ]  Square Bracket
+        //   Used For Comment only
+         ;   Semi Colon
+        ( )  Round Bracket
+        < >  Angle Brackets or Chevrons
+        " "  Double Quote
+
+     */
+
+        //Main Method
+    public static void main(String[] args) {
+
+        //To Print
+     System.out.println("This is Demo Project For Beginners");
+    }
+
+}
